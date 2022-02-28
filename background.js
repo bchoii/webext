817f3e758a05db1a5c5ebcd1455de667a3bfa3d0
Bernard Choi <bchoii@gmail.com>
2022-02-28 10:16:43 +0800

    .

--- background.js
+++ background.js
@@ -1,34 +1,40 @@
 chrome.tabs.onCreated.addListener((newTab) => {
-    try {
-        chrome.tabs.move(newTab.id, {index:-1});    
-    } catch (error) {
-    }
-})
+  try {
+    chrome.tabs.move(newTab.id, { index: -1 });
+  } catch (error) {}
+});
+
+const closeTab = (tab) => chrome.tabs.remove(tab.id);
 
 const moveTabToEnd = async (currentTab) => {
-    const currentIndex = currentTab.index;
-    const tabs = await chrome.tabs.query({currentWindow:true});
-    chrome.tabs.move(currentTab.id, {index:-1});
-    try {
-        chrome.tabs.update(tabs[currentIndex + 1].id, {active:true});
-    } catch (error) {
-        chrome.tabs.update(tabs[0].id, {active:true});
-    }
-}
+  const currentIndex = currentTab.index;
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  chrome.tabs.move(currentTab.id, { index: -1 });
+  try {
+    chrome.tabs.update(tabs[currentIndex + 1].id, { active: true });
+  } catch (error) {
+    chrome.tabs.update(tabs[0].id, { active: true });
+  }
+};
 
 const getCurrentTab = async () => {
-    const activeTabs = await chrome.tabs.query({active:true,currentWindow:true});
-    return activeTabs[0];
-}
+  const activeTabs = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  return activeTabs[0];
+};
 
 chrome.commands.onCommand.addListener(async (command) => {
-    const currentTab = await getCurrentTab();
-    switch (command) {
-        case "move-tab-to-end":
-            await moveTabToEnd(currentTab);            
-            break;
-        default:
-            break;
-    }
-})
-
+  const currentTab = await getCurrentTab();
+  switch (command) {
+    case 'close-tab':
+      await closeTab(currentTab);
+      break;
+    case 'move-tab-to-end':
+      await moveTabToEnd(currentTab);
+      break;
+    default:
+      break;
+  }
+});
--- contentscript.js
+++ contentscript.js
@@ -1,27 +1,63 @@
-const videos = document.getElementsByTagName("video");
+const videos = document.getElementsByTagName('video');
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
 const _incVideoTime = (inc) => {
-    for (const video of videos) {
-        video.currentTime += inc;
+  for (const video of videos) {
+    video.currentTime += inc;
+  }
+};
+
+const _setVideoRate = (rate) => {
+  for (const video of videos) {
+    if (rate < 32) {
+      video.playbackRate = rate;
     }
-}
+  }
+};
+
+const _incVideoRate = (inc) => {
+  for (const video of videos) {
+    _setVideoRate(video.playbackRate * inc);
+  }
+};
+
+const _incVideoVolume = (inc) => {
+  for (const video of videos) {
+    video.volume = clamp(video.volume + inc, 0, 1);
+  }
+};
 
 document.onkeydown = (event) => {
-    switch (event.key) {
-        case 'z':
-            _incVideoTime(-3);
-            break;
-        case 'x':
-            _incVideoTime(5);
-            break;
-        case 'v':
-            _incVideoTime(30);
-            break;
-        case 'b':
-            _incVideoTime(60);
-            break;
-        default:
-            break;
-    }
-}
-
+  switch (event.key) {
+    case 'q':
+      _incVideoVolume(-0.1);
+      break;
+    case 'w':
+      _incVideoVolume(0.1);
+      break;
+    case 'a':
+      _setVideoRate(1);
+      break;
+    case 's':
+      _incVideoRate(0.5);
+      break;
+    case 'd':
+      _incVideoRate(2);
+      break;
+    case 'z':
+      _incVideoTime(-3);
+      break;
+    case 'x':
+      _incVideoTime(5);
+      break;
+    case 'v':
+      _incVideoTime(30);
+      break;
+    case 'b':
+      _incVideoTime(60);
+      break;
+    default:
+      break;
+  }
+};
